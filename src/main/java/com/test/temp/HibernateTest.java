23348bedb30d8ba8214784e827bb6eb1d35e7bb9
wangyongtao <dayiwyt@sina.com>
2020-05-08 07:55:27 +0800

    new one java file

---
+++ src/main/java/com/test/temp/HibernateTest.java
@@ -0,0 +1,14 @@
+package com.test.temp;
+
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @Author : wangyongtao
+ * @Description : TODO
+ * @Date : 2020/5/7 0007 20:39
+ **/
+public class HibernateTest {
+    public static void main(String[] args) {
+        Configuration cfg = new Configuration().configure();
+    }
+}
