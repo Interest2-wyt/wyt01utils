cb20ac6cd170cbc02c297d14df28c7805e2cc9e1
Interest2-wyt <dayiwyt@sina.com>
2020-08-22 20:07:14 +0800

    update excel

--- src/main/java/com/test/characterencode/CharacterEncode.java
+++ src/main/java/com/test/characterencode/CharacterEncode.java
@@ -1,9 +1,44 @@
 package com.test.characterencode;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.nio.charset.Charset;
+
 /**
  * @Author : wangyongtao
- * @Description : TODO
+ * @Description : 字符乱码测试
  * @Date : 2020/8/8 0008 21:30
  **/
 public class CharacterEncode {
+
+    public static void main(String[] args) {
+        String source = "中文测试";
+        Charset gbkCharset = Charset.forName("gbk");
+        Charset utf8Charset = Charset.forName("utf-8");
+        Charset iso88591Charset = Charset.forName("iso-8859-1");
+
+        Charset defaultCharset = Charset.defaultCharset();
+
+        System.out.printf("defaultCharset:%s%n", defaultCharset);
+
+        System.out.println(StringUtils.repeat("=",20));
+
+        String str1 = StringUtils.toEncodedString(source.getBytes(gbkCharset), utf8Charset);
+        System.out.printf("gbk=>utf-8：%s%n", str1);
+
+        String str4 = StringUtils.toEncodedString(source.getBytes(utf8Charset), gbkCharset);
+        System.out.printf("utf-8=>gbk：%s%n", str4);
+
+        String str2 = StringUtils.toEncodedString(source.getBytes(iso88591Charset), utf8Charset);
+        System.out.printf("iso8859-1=>utf-8：%s%n", str2);
+
+        String str5 = StringUtils.toEncodedString(source.getBytes(utf8Charset), iso88591Charset);
+        System.out.printf("utf-8=>iso8859-1：%s%n", str5);
+
+        String str3 = StringUtils.toEncodedString(source.getBytes(gbkCharset), iso88591Charset);
+        System.out.printf("gbk=>iso8859-1：%s%n", str3);
+
+        String str6 = StringUtils.toEncodedString(source.getBytes(iso88591Charset), gbkCharset);
+        System.out.printf("iso8859-1=>gbk：%s%n", str6);
+    }
 }
--- src/main/java/com/test/officejava/DateExportService.java
+++ src/main/java/com/test/officejava/DateExportService.java
@@ -7,13 +7,18 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.junit.Test;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
 
 @Service
 public class DateExportService {
 
-
+    /**
+     * 原始excel导出方法
+     */
     @Test
     public void getDateExport(){
         HSSFWorkbook workbook = new HSSFWorkbook();
@@ -47,5 +52,84 @@ public class DateExportService {
 
 //        return null;
     }
+
+    /**
+     * 获取要导出的报表对象
+     * @param sheetName sheet名称
+     * @param title 标题
+     * @param values 内容
+     * @param wb HSSFWorkbook对象
+     * @return
+     */
+    public static HSSFWorkbook getHSSFWorkbook(String sheetName,String []title,String [][]values,HSSFWorkbook wb){
+        // 第一步，创建一个HSSFWorkbook，对应一个Excel文件
+        if(wb == null){
+            wb = new HSSFWorkbook();
+        }
+
+        // 第二步，在workbook中添加一个sheet,对应Excel文件中的sheet
+        HSSFSheet sheet = wb.createSheet(sheetName);
+
+        // 第三步，在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制
+        HSSFRow row = sheet.createRow(0);
+
+        // 第四步，创建单元格，并设置值表头 设置表头居中
+//        HSSFCellStyle style = wb.createCellStyle();
+//        style.setAlignment(HSSFCellStyle.ALIGN_CENTER); // 创建一个居中格式
+
+        //声明列对象
+        HSSFCell cell = null;
+
+        //创建标题
+        for(int i=0;i<title.length;i++){
+            cell = row.createCell(i);
+            cell.setCellValue(title[i]);
+//            cell.setCellStyle(style);
+        }
+
+        //创建内容
+        for(int i=0;i<values.length;i++){
+            row = sheet.createRow(i + 1);
+            for(int j=0;j<values[i].length;j++){
+                //将内容按顺序赋给对应的列对象
+                row.createCell(j).setCellValue(values[i][j]);
+            }
+        }
+        return wb;
+    }
+
+    /**
+     * 导出报表
+     * @param workbook 要导出的报表对象
+     * @param excelName 导出的excel名称
+     * @param response
+     */
+    public void exportReportForm(HSSFWorkbook workbook,String excelName,HttpServletResponse response) {
+        //4、将excel表通过浏览器输出
+        if (workbook!=null){
+            OutputStream os = null;
+            try {
+                response.setContentType("application/binary;charset=ISO8859-1");
+                response.setHeader("Content-Disposition","attachment;filename=\"" + URLEncoder.encode
+                        (excelName, "UTF-8") + "\"");
+                os = response.getOutputStream();
+                workbook.write(os);
+                os.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if (null != os) {
+                    try {
+                        os.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }else {
+            System.out.println("表格为空");
+        }
+    }
+
 
 }
